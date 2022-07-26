400d63f92e9fcf679ae29f7105b87172500cd8c2
Elizabeth Alcalá <elizabethalcala28@gmail.com>
2022-07-26 17:13:51 -0500

    adjust styles

--- src/components/standalone-cta/standalone-cta.tsx
+++ src/components/standalone-cta/standalone-cta.tsx
@@ -1,20 +1,18 @@
-import Text from '../text'
-import { LogoIcon } from '../../icons/logo'
-import './standalone-cta.css'
+import Text from "../text";
+import { LogoIcon } from "../../icons/logo";
+import "./standalone-cta.css";
 
 export const StandaloneCTA = () => {
-	return (
-		<div className="cta-container">
-			<div className='title'>
-				<Text type="titleSm">Get more in Sonar Studio</Text>
-				<LogoIcon />
-			</div>
-			<Text className='description'>Discover the next-gen crypto tools as we are building them out</Text>
-			<div className='button'>
-				<div>
-					Try the Beta NOW
-				</div>
-			</div>
-		</div>
-	)
-}
+  return (
+    <div className="cta-container">
+      <div className="title">
+        <Text type="titleSm">Get more in Sonar Studio</Text>
+        <LogoIcon />
+      </div>
+      <Text className="description">
+        Discover the next-gen crypto tools as we are building them out
+      </Text>
+      <button>Try the Beta NOW</button>
+    </div>
+  );
+};
